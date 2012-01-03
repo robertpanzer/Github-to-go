c6418565b9f0f0babd529d751fb37842508d4e5a
Robert Panzer <robertpanzer@Roberts-MacBook-Air.local>
2012-01-03 13:56:24 +0100

    Initial Commit

---
+++ Github To Go/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Github To Go
+//
+//  Created by Robert Panzer on 03.01.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
